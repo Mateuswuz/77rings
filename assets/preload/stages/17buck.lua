d45dd5ebf6ac09ef3d760c67fb312831ac3c14c3
Mateuzinholol <116360372+Mateuswuz@users.noreply.github.com>
2024-03-20 19:16:44 -0300

    Add files via upload

---
+++ assets/preload/stages/17buck.lua
@@ -0,0 +1,37 @@
+function onCreate() 
+
+precacheImage('sonicLoad')
+
+	   makeLuaSprite('rizz', 'White', -2500, -2500)
+       scaleObject('rizz', 30, 30)
+	      addLuaSprite('rizz', false)
+		  
+	function onUpdate(elapsed)
+	if curStep == 528 then
+	
+	setProperty('rizz.visible',false)
+	
+	function onUpdate(elapsed)
+	if curStep == 784 then
+	
+	setProperty('rizz.visible',true)
+	
+	function onUpdate(elapsed)
+	if curStep == 1040 then
+	
+	setProperty('rizz.visible',false)
+	
+	function onUpdate(elapsed)
+	if curStep == 1168 then
+	
+	setProperty('rizz.visible',true)
+
+end
+end
+end
+end
+end
+end
+end
+end
+end
